18d6c961a5dc065a59a2691c533645d2191daf6c
BrianFeurstein <79904813+BrianFeurstein@users.noreply.github.com>
2022-03-15 09:02:09 +0100

    AccessToApi_earth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AccessToApi_weather from './Components/AccessToApi_weather';
+import AccessToApi_earth from './Components/AccessToApi_earth';
 
 function App() {
   return (
     <div className="App">
       <AccessToApi_weather/>
+      <AccessToApi_earth/>
+      
     </div>
   );
 }
